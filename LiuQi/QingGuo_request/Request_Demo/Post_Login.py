4cdc3f8fdae3dffd7f54e7c996dc57ffa873bf5a
gaya0214 <13180686518@163.com>
2018-12-19 14:53:55 +0800

    QingGuo_request

---
+++ LiuQi/QingGuo_request/Request_Demo/Post_Login.py
@@ -0,0 +1,61 @@
+import unittest
+from QingGuo.Utils.Sendhttp import SendHttp
+from QingGuo.Utils.DataProvider import readExcel
+
+
+class qgLoginTest(unittest.TestCase):
+    def setUp(self):
+        self.url="/common/fgadmin/login"
+    def test_login_by_para(self):
+        sheet =readExcel(r"D:\demo\userdata.xlsx", 0)
+        for i in range(sheet.nrows):
+            user = {"phoneArea": "86", "phoneNumber": sheet.cell_value(i, 0), "password": "netease123"}
+            result = SendHttp().run_http(self.url, "POST", user)
+            print(result)
+
+    def test_login_success(self):
+        user={"phoneArea":"86","phoneNumber":"20000000000","password":"netease123"}
+        result=SendHttp().run_http(self.url, "POST", user)
+        print ( result )
+    def test_login_fail1(self):
+        user={"phoneArea":86,"phoneNumber":"20000000000","password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print(result)
+    def test_login_fail2(self):
+        user={"phoneArea":"86","phoneNumber":"20000000000","password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail2(self):
+        user={"phoneArea":"86","phoneNumber":20000000000,"password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print(result)
+    def test_login_fail3(self):
+        user={"phoneArea":"86","phoneNumber":"20000000000","password":123456}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail4(self):
+        user={"phoneNumber":"20000000000","password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print(result)
+    def test_login_fail5(self):
+        user={"phoneArea":"86","password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail6(self):
+        user={"phoneArea":"86","phoneNumber":"20000000000"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail7(self):
+        user={"phoneArea":"86","phoneNumber":"123456789012","password":"netease123"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail8(self):
+        user={"phoneArea":"86","phoneNumber":"20000000000","password":"wrong"}
+        result=SendHttp().run_http(self.url,"POST",user)
+        print ( result )
+    def test_login_fail9(self):
+        user = {"phoneArea": "86", "phoneNumber": "10086", "password": "netease123"}
+        result = SendHttp().run_http(self.url, "POST", user )
+        print ( result )
+if __name__=='__main__':
+    unittest.main()
---
+++ LiuQi/QingGuo_request/Request_Demo/Scene1.py
@@ -0,0 +1,22 @@
+import unittest
+from QingGuo.Utils.Sendhttp import SendHttp
+class Scence1(unittest.TestCase):
+    def setUp(self):
+        self.url = "/common/skuList"
+    def test_skulist_success1(self):
+        result = SendHttp().sent_get(self.url)
+        print(result)
+    def test_skulist_success2(self):
+        Id = {"goodsId": 1}
+        result = SendHttp().sent_get(self.url, Id)
+        print(result)
+    def test_skulist_success3(self):
+        Id = {"goodsId": 2}
+        result = SendHttp().sent_get(self.url, Id)
+        print(result)
+    def test_skulist_success4(self):
+        Id = {"goodsId": 3}
+        result = SendHttp().sent_get(self.url, Id)
+        print(result)
+if __name__=='__main__':
+    unittest.main()
