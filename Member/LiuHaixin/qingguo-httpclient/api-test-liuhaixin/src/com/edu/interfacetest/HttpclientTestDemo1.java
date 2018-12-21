3dad617b930cb57be2bcaf36b46237c6e2627a29
liuhaixin <1689449711@qq.com>
2018-12-21 09:03:49 +0800

    new

--- Member/LiuHaixin/qingguo-httpclient/api-test-liuhaixin/src/com/edu/interfacetest/HttpclientTestDemo1.java
+++ Member/LiuHaixin/qingguo-httpclient/api-test-liuhaixin/src/com/edu/interfacetest/HttpclientTestDemo1.java
@@ -1,45 +1,107 @@
 package com.edu.interfacetest;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
-import com.edu.core.BaseTest;
-import com.edu.core.HttpDriver;
-import com.edu.utils.Checker;
+import com.edu.core.HttpClient;
 
-public class HttpclientTestDemo1 extends BaseTest{
-	Checker check;
-	String subUrl = "/common/skuList/";
-	String result=null;
+public class HttpclientTestDemo1 extends HttpClient{
+	
+   	CloseableHttpClient httpclient=null;
+	CloseableHttpResponse response;
+	
+  	@Test
+  	public void TestSkuList1() throws Exception {
+  		try {
+  			httpclient=HttpClients.createDefault();
+  			HttpGet httpGet=new HttpGet("http://study-perf.qa.netease.com/common/skuList");
+  			response = httpclient.execute(httpGet);
+  			System.out.println(response.getStatusLine());
+  			httpGet.setHeader("Content-Type", "application/json");
+  			HttpEntity entity=response.getEntity();
+  			if(entity !=null) {
+  				System.out.println(EntityUtils.toString(entity,"UTF-8"));
+  			}
+  			EntityUtils.consume(entity);
+  		}catch(Exception e) {
+  			e.printStackTrace();
+  		}finally {
+  			response.close();
+  			httpclient.close();
+  		}	
+  	}
 
-	public String skuList() throws Exception {
-		 result = HttpDriver.doGet(subUrl);
-		check = new Checker(result);
-		return result;
-	}
-	
-	public String skuListById(String goodsId) throws Exception {
-	
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("goodsId", goodsId);
-		map.put("addressDetail", "");
-		String result = HttpDriver.doGet(subUrl,map);
-		check = new Checker(result);
-		return result;
-	}
-	@Test
-	public void TestSkuList1() throws Exception {
-		result = skuList();
-		check.verifyTextPresent("mesage");
-	}
-	public void TestSkuList2() throws Exception {
-		result = skuList();
-		check.verifyTextPresent("mesage");
-	}
-	public void TestSkuList3() throws Exception {
-		result = skuList();
-		check.verifyTextPresent("mesage");
-	}
+
+  	@Test
+  	public void TestSkuList2() throws Exception {
+  		try {
+  			httpclient=HttpClients.createDefault();
+  			HttpGet httpGet=new HttpGet("http://study-perf.qa.netease.com/common/skuList?goodsId=1");
+  			response = httpclient.execute(httpGet);
+  			System.out.println(response.getStatusLine());
+  			httpGet.setHeader("Content-Type", "application/json");
+  			HttpEntity entity=response.getEntity();
+  			if(entity !=null) {
+  				System.out.println(EntityUtils.toString(entity,"UTF-8"));
+  			}
+  			EntityUtils.consume(entity);
+  		}catch(Exception e) {
+  			e.printStackTrace();
+  		}finally {
+  			response.close();
+  			httpclient.close();
+  		}	
+  	}
+    
+
+
+  	@Test
+  	public void TestSkuList3() throws Exception {
+  		try {
+  			httpclient=HttpClients.createDefault();
+  			HttpGet httpGet=new HttpGet("http://study-perf.qa.netease.com/common/skuList?goodsId=2");
+  			response = httpclient.execute(httpGet);
+  			System.out.println(response.getStatusLine());
+  			httpGet.setHeader("Content-Type", "application/json");
+  			HttpEntity entity=response.getEntity();
+  			if(entity !=null) {
+  				System.out.println(EntityUtils.toString(entity,"UTF-8"));
+  			}
+  			EntityUtils.consume(entity);
+  		}catch(Exception e) {
+  			e.printStackTrace();
+  		}finally {
+  			response.close();
+  			httpclient.close();
+  		}	
+  	}
+
+
+
+  	@Test
+  	public void TestSkuList4() throws Exception {
+  		try {
+  			httpclient=HttpClients.createDefault();
+  			HttpGet httpGet=new HttpGet("http://study-perf.qa.netease.com/common/skuList?goodsId=3");
+  			response = httpclient.execute(httpGet);
+  			System.out.println(response.getStatusLine());
+  			httpGet.setHeader("Content-Type", "application/json");
+  			HttpEntity entity=response.getEntity();
+  			if(entity !=null) {
+  				System.out.println(EntityUtils.toString(entity,"UTF-8"));
+  			}
+  			EntityUtils.consume(entity);
+  		}catch(Exception e) {
+  			e.printStackTrace();
+  		}finally {
+  			response.close();
+  			httpclient.close();
+  		}	
+  	}
+    
 }
